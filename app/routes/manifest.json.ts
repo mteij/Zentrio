76fa1d3ed30abf9e0672b198ed2f7f4c8c931587
Michiel Eijpe <mail@mteij.nl>
2025-07-27 19:24:26 +0200

    Fix rotation error

--- app/routes/manifest.json.ts
+++ app/routes/manifest.json.ts
@@ -3,7 +3,7 @@ import { Handlers } from "$fresh/server.ts";
 export const handler: Handlers = {
   GET: (req) => {
     const url = new URL(req.url);
-    const orientation = url.searchParams.get("orientation") || "auto";
+    const orientation = url.searchParams.get("orientation") || "any";
 
     const manifest = {
       "name": "Zentrio",
