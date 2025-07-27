d667741f8a56c96154a4e7b2eb8764990abc46a7
Michiel Eijpe <mail@mteij.nl>
2025-07-27 23:17:30 +0200

    404 not found

--- app/fresh.gen.ts
+++ app/fresh.gen.ts
@@ -2,6 +2,7 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
+import * as $_404 from "./routes/_404.tsx";
 import * as $_app from "./routes/_app.tsx";
 import * as $_middleware from "./routes/_middleware.ts";
 import * as $api_addon_manager from "./routes/api/addon-manager.ts";
@@ -52,6 +53,7 @@ import { type Manifest } from "$fresh/server.ts";
 
 const manifest = {
   routes: {
+    "./routes/_404.tsx": $_404,
     "./routes/_app.tsx": $_app,
     "./routes/_middleware.ts": $_middleware,
     "./routes/api/addon-manager.ts": $api_addon_manager,
