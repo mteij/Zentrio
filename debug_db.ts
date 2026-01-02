cc98491c6be3c704bc938a050a408cc7e67506cb
Michiel Eijpe <mail@mteij.nl>
2026-01-02 14:53:06 +0100

    Remove debug file

--- debug_db.ts
+++
@@ -1,8 +0,0 @@
-
-import { Database } from 'bun:sqlite';
-
-const db = new Database('app/data/zentrio.db');
-
-console.log('--- Watch History Dump ---');
-const rows = db.query('SELECT * FROM watch_history ORDER BY updated_at DESC LIMIT 20').all();
-console.log(JSON.stringify(rows, null, 2));
